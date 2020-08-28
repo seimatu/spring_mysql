716f19e948b805e0e32f6e98743fe9f69ce890c5
s_matsumoto <s_matsumoto@eruesupi00420noMacBook-Air.local>
2020-08-28 11:19:13 +0900

    add_CRUD

--- src/main/java/com/example/dbtest/controller/MainController.java
+++ src/main/java/com/example/dbtest/controller/MainController.java
@@ -2,6 +2,8 @@ package com.example.dbtest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import com.example.dbtest.entity.Idol;
@@ -19,4 +21,25 @@ public class MainController {
 		mav.addObject("data", list);
 		return mav;
 	}
+
+	@RequestMapping("/insert")
+	@Transactional(readOnly = false)
+	private ModelAndView insert(@ModelAttribute("formInsert") Idol idol, ModelAndView mav) {
+		repository.saveAndFlush(idol);
+		return new ModelAndView("redirect:/");
+	}
+
+	@RequestMapping("/update")
+	@Transactional(readOnly = false)
+	private ModelAndView update(@ModelAttribute("formUpdate") Idol idol, ModelAndView mav) {
+		repository.saveAndFlush(idol);
+		return new ModelAndView("redirect:/");
+	}
+	@RequestMapping("/delete")
+	@Transactional(readOnly = false)
+	private ModelAndView delete(@ModelAttribute("formDelete") Idol idol, ModelAndView mav) {
+		repository.delete(idol);
+		return new ModelAndView("redirect:/");
+	}
+	
 }
--- src/main/java/com/example/dbtest/entity/Idol.java
+++ src/main/java/com/example/dbtest/entity/Idol.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 @Table(name = "idol")
 public class Idol {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     @Column(name="age")
